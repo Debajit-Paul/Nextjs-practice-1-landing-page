a12dec112ea290848e003b62b10abd9f22b12279
Debajit Paul <62774980+Debajit-Paul@users.noreply.github.com>
2024-03-27 15:04:03 -0700

    initial commit

---
+++ my-app/components/Button.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  varient: string;
+};
+const Button = ({ type, varient, icon, title }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${varient}`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
+};
+
+export default Button;
